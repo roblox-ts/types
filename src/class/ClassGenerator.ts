45ea24d759bd848f9e65946da31bc0a4ff897c33
osyrisrblx <osyrisrblx@gmail.com>
2018-09-16 01:24:12 -0400

    add new line in class generator

--- src/class/ClassGenerator.ts
+++ src/class/ClassGenerator.ts
@@ -309,6 +309,7 @@ export class ClassGenerator extends Generator {
 		const project = new Project({ tsConfigFilePath: path.join(__dirname, "..", "..", "include", "tsconfig.json") });
 		const sourceFile = project.getSourceFileOrThrow("manual.d.ts");
 		this.write("// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED BY HAND!");
+		this.write("");
 		this.write('/// <reference path="generated_enums.d.ts" />');
 		this.write('/// <reference path="manual.d.ts" />');
 		this.write("");
