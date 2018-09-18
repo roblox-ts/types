3a244a27ce2f155c755eb9192dd136aad792627f
osyrisrblx <osyrisrblx@gmail.com>
2018-09-18 01:59:55 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ git config --global user.name "Travis CI";
 git checkout master;
 node out/index.js;
 if ! git diff-index --quiet HEAD --; then
-	npm version patch -m "Bumping to %s [ci skip]";
 	echo "changes detected, publishing..";
+	npm version patch -m "Bumping to %s [ci skip]";
 	git push https://${GIT_TOKEN}@github.com/roblox-ts/rbx-types.git;
 	npm publish;
 fi;
