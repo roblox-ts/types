51a88c8960ac45d4ce849a1c25b660938fdf0191
Osyris <osyrisrblx@gmail.com>
2018-10-18 02:52:45 -0400

    add missing method JSDoc comments to manaul.d.ts

---
+++ src/scripts/update_manual.ts
@@ -0,0 +1,39 @@
+import * as path from "path";
+import axios from "axios";
+import Project, * as ts from "ts-simple-ast";
+import { ReflectionMetadata } from "../class/ReflectionMetadata";
+
+const BASE_URL = "https://raw.githubusercontent.com/CloneTrooper1019/Roblox-Client-Watch/roblox/";
+const REFLECTION_METADATA_URL = BASE_URL + "ReflectionMetadata.xml";
+
+(async () => {
+	const response = await axios.get(REFLECTION_METADATA_URL);
+	const metadata = new ReflectionMetadata(response.data);
+	const project = new Project({ tsConfigFilePath: path.join(__dirname, "..", "..", "include", "tsconfig.json") });
+	const sourceFile = project.getSourceFileOrThrow("manual.d.ts");
+	for (const statement of sourceFile.getStatements()) {
+		if (ts.TypeGuards.isInterfaceDeclaration(statement)) {
+			let className = statement.getName();
+			if (className.startsWith("Rbx_")) {
+				className = className.substr(4);
+				for (const member of statement.getMembers()) {
+					if (member.getJsDocs().length === 0) {
+						let description: string | undefined = undefined;
+						if (ts.TypeGuards.isMethodSignature(member)) {
+							description = metadata.getMethodDescription(className, member.getName());
+						} else if (ts.TypeGuards.isPropertySignature(member)) {
+							description =
+								metadata.getPropertyDescription(className, member.getName()) ||
+								metadata.getCallbackDescription(className, member.getName()) ||
+								metadata.getEventDescription(className, member.getName());
+						}
+						if (description) {
+							member.addJsDoc(description);
+						}
+					}
+				}
+			}
+		}
+	}
+	await sourceFile.save();
+})();
