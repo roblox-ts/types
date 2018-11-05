ba9084d6c88f22ed0afd2a979cd67890c18f704a
Osyris <osyrisrblx@gmail.com>
2018-11-05 00:52:42 -0500

    add Array.find

--- include/es.d.ts
+++ include/es.d.ts
@@ -208,6 +208,18 @@ interface Array<T> {
 		initialValue: U
 	): U;
 
+	/**
+	 * Returns the value of the first element in the array where predicate is true, and undefined
+	 * otherwise.
+	 * @param predicate find calls predicate once for each element of the array, in ascending
+	 * order, until it finds one where predicate returns true. If such an element is found, find
+	 * immediately returns that element value. Otherwise, find returns undefined.
+	 * @param thisArg If provided, it will be used as the this value for each invocation of
+	 * predicate. If it is not provided, undefined is used instead.
+	 */
+	find<S extends T>(predicate: (value: T, index: number, obj: T[]) => value is S): S | undefined;
+	find(predicate: (value: T, index: number, obj: T[]) => boolean): T | undefined;
+
 	[n: number]: T;
 }
 
