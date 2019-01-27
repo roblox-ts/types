e2e64cb3eef1c8cead2a6a5bcb6273fba78f8c05
Travis CI <travis@travis-ci.org>
2019-01-27 23:10:23 +0000

    Manual Update

--- include/manual.d.ts
+++ include/manual.d.ts
@@ -343,6 +343,8 @@ interface Rbx_InsertService extends Rbx_Instance {
 }
 
 interface Rbx_Instance {
+	/** Returns a copy of this Object and all its children. The copy's Parent is nil */
+	Clone(): this;
 	/** Returns an array containing all of the descendants of the instance. Returns in preorder traversal, or in other words, where the parents come before their children, depth first. */
 	GetDescendants(): Array<Instance>;
 	/** Returns the first ancestor of this Instance that matches the first argument 'name'.  The function will return nil if no Instance is found. */
