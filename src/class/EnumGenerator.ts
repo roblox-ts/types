92dc2a358063af856f9de5e71df0c4f0355a22be
Osyris <osyrisrblx@gmail.com>
2019-02-01 20:55:05 -0500

    Revert "Merge branch 'constructor-rework' into master"
    This reverts commit 7eb490848c726d0f9ca04591de92ed780d42cb77.

--- src/class/EnumGenerator.ts
+++ src/class/EnumGenerator.ts
@@ -2,15 +2,15 @@ import { Generator } from "./Generator";
 
 export class EnumGenerator extends Generator {
 	public async generate(rbxEnums: Array<ApiEnum>) {
-		this.write(`// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED BY HAND!`);
-		this.write(``);
+		this.write("// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED BY HAND!");
+		this.write("");
 		this.write('/// <reference no-default-lib="true"/>');
 		this.write('/// <reference path="roblox.d.ts" />');
 		this.write('/// <reference path="manual.d.ts" />');
 		this.write('/// <reference path="generated_classes.d.ts" />');
-		this.write(``);
+		this.write("");
 		this.write(`// GENERATED ROBLOX ENUMS`);
-		this.write(``);
+		this.write("");
 		this.write(`declare namespace Enum {`);
 		this.pushIndent();
 		for (let i = 0; i < rbxEnums.length; i++) {
