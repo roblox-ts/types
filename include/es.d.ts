18ec398c5130180ef135e98ba5303cc31fe8cfa8
Travis CI <travis@travis-ci.org>
2019-02-14 01:55:46 +0000

    Manual Update

--- include/es.d.ts
+++ include/es.d.ts
@@ -656,6 +656,14 @@ interface PromiseConstructor {
 	 * @param promises An array of Promises.
 	 */
 	all: <T>(promises: Array<Promise<T>>) => Promise<Array<T>>;
+
+	/**
+	 * Spawns a function on a new thread, but begins running it immediately
+	 * instead of being deferred. This is sometimes known as a "fast spawn".
+	 * Should be preferred over `spawn` in Promises for more predictable timing.
+	 * @param callback The function to call. Any further arguments are passed as parameters.
+	 */
+	spawn: <T extends any[]>(callback: (...args: T) => void, ...args: T) => void
 }
 
 declare var Promise: PromiseConstructor;
