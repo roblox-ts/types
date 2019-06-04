ec1ba739c357f46d480f3f7b7682955d307e64f2
osyrisrblx <osyrisrblx@gmail.com>
2019-06-03 19:48:17 -0700

    fix #83

--- include/es.d.ts
+++ include/es.d.ts
@@ -683,13 +683,13 @@ declare const WeakSet: WeakSetConstructor;
 interface PromiseLike<T> {
 	/**
 	 * Attaches callbacks for the resolution and/or rejection of the Promise.
-	 * @param onfulfilled The callback to execute when the Promise is resolved.
-	 * @param onrejected The callback to execute when the Promise is rejected.
+	 * @param onResolved The callback to execute when the Promise is resolved.
+	 * @param onRejected The callback to execute when the Promise is rejected.
 	 * @returns A Promise for the completion of which ever callback is executed.
 	 */
 	then<TResult1 = T, TResult2 = never>(
-		onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
-		onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null,
+		onResolved?: ((value: T) => TResult1 | PromiseLike<TResult1>) | void,
+		onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | void,
 	): PromiseLike<TResult1 | TResult2>;
 }
 
