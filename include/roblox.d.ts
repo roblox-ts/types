a543689fb61e4e77f46bd033f8ba4d8487351f33
Validark <NarrevRBX@gmail.com>
2019-06-08 08:56:09 -0500

    Add ChangedSignal type

--- include/roblox.d.ts
+++ include/roblox.d.ts
@@ -6,6 +6,29 @@
 /// <reference path="generated/None.d.ts" />
 
 // ROBLOX API
+/**
+ * Intersecting a type with this allows access to the `Instance.Changed` event.
+ *
+ * @example
+ * function f(p: Part) {
+ * 	(p as Part & ChangedSignal).Changed.Connect(() => {})
+ * }
+ */
+type ChangedSignal = {
+	/**
+	 * If you want to detect changes of a single property only, you should use [Instance.GetPropertyChangedSignal](https://developer.roblox.com/api-reference/function/Instance/GetPropertyChangedSignal) instead!
+	 *
+	 * The Changed event fires right after most properties change on objects. It is possible to find the present value of a changed property by using `object[property]`. To get the value of a property before it changes, you must have stored the value of the property before it changed.
+	 *
+	 * If you are only interested in listening to the change of a specific property, consider using the `GetPropertyChangedSignal` method instead to get an event that only fires when a given property changes.
+	 *
+	 * This event does not fire for physics-related changes, like when the `CFrame`, `Velocity`, `RotVelocity`, `Position`, `Orientation` and `CFrame` properties of a `BasePart` change due to gravity. To detect changes in these properties, consider using a physics-based event like `RunService.Stepped` or `BasePart.Touched`. A while-true-do loop can also work.
+	 *
+	 * For "-Value" objects, this event behaves differently: it only fires when the `Value` property changes. See individual pages for `IntValue`, `StringValue`, etc for more information. To detect other changes in these objects, you must use `GetPropertyChangedSignal` instead.
+	 */
+	readonly Changed: RBXScriptSignal<(value: string) => void>;
+};
+
 type StrictInstances = {
 	[Key in keyof Instances]: Instances[Key]["ClassName"] extends Key
 		? Instances[Key]
