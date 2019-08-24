af4e184a2d6f46c5e9fa1f570b4a9bfb1c4f518a
Travis CI <travis@travis-ci.org>
2019-08-24 20:58:36 +0000

    Manual Update

--- include/lua.d.ts
+++ include/lua.d.ts
@@ -13,7 +13,7 @@ declare const _G: Table;
 declare const _VERSION: string;
 
 /** Throws an error if the provided value is false or nil . Returns the value that was passed into the function if the assertion passes. */
-declare function assert<T>(condition: T, message?: string): T;
+declare function assert<T>(condition: T, message?: string): Exclude<T, false | undefined>;
 
 /** Performs an operation on the Lua garbage collector based on the specified option. Roblox's Lua sandbox only allows the "count" option to be used, so none of the other standard options are available. The "count" option returns the total memory in use by Lua (in kilobytes). */
 declare function collectgarbage(option: "count"): number;
