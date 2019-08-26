f7af72259b37c2663d255a2300cf6044c7f9f72a
Travis CI <travis@travis-ci.org>
2019-08-26 18:50:13 +0000

    Manual Update

--- include/lua.d.ts
+++ include/lua.d.ts
@@ -333,6 +333,105 @@ declare namespace math {
 	function tanh(n: number): number;
 }
 
+/**
+ * Unless otherwise stated, all functions accept numeric arguments in the range `(-2^51, +2^51)`;
+ * each argument is normalized to the remainder of its division by `2^32` and truncated to an integer
+ * (in some unspecified way), so that its final value falls in the range `[0, 2^32 - 1]`.
+ * Similarly, all results are in the range `[0, 2^32 - 1]`.
+ * Note that `bit32.bnot(0)` is `0xFFFFFFFF`, which is different from -1.
+ */
+declare namespace bit32 {
+	/**
+	 * Returns the number `x` shifted `disp` bits to the right. The number `disp` may be any representable integer.
+	 * Negative displacements shift to the left. This shift operation is what is called arithmetic shift.
+	 * Vacant bits on the left are filled with copies of the higher bit of `x`; vacant bits on the right are filled
+	 * with zeros. In particular, displacements with absolute values higher than 31 result in zero or `0xFFFFFFFF`
+	 * (all original bits are shifted out).
+	 */
+	function arshift(x: number, disp: number): number;
+
+	/** Returns the bitwise _and_ of its operands. */
+	function band(...operands: Array<number>): number;
+
+	/**
+	 * Returns the bitwise negation of `x`. For any integer `x`, the following identity holds:
+	 *
+	 * `assert(bit32.bnot(x) == (-1 - x) % 2^32)`
+	 */
+	function bnot(x: number): number;
+
+	/** Returns the bitwise _or_ of its operands. */
+	function bor(...operands: Array<number>): number;
+
+	/** Returns a boolean signaling whether the bitwise _and_ of its operands is different from zero. */
+	function btest(...operands: Array<number>): boolean;
+
+	/** Returns the bitwise _exclusive or_ of its operands. */
+	function bxor(...operands: Array<number>): boolean;
+
+	/**
+	 * Returns the unsigned number formed by the bits `field` to `field + width - 1` from `n`.
+	 * Bits are numbered from 0 (least significant) to 31 (most significant).
+	 * All accessed bits must be in the range `[0, 31]`.
+	 *
+	 * The default for `width` is 1.
+	 */
+	function extract(n: number, field: number, width?: number): number;
+
+	/**
+	 * Returns a copy of `n` with the bits `field` to `field + width - 1` replaced by the value `v`.
+	 *
+	 * See `bit32.extract` for details about `field` and `width`.
+	 */
+	function replace(n: number, v: number, field: number, width?: number): number;
+
+	/**
+	 * Returns the number `x` rotated `disp` bits to the left. The number `disp` may be any representable integer.
+	 *
+	 * For any valid displacement, the following identity holds:
+	 *
+	 * `assert(bit32.lrotate(x, disp) == bit32.lrotate(x, disp % 32))`
+	 *
+	 * In particular, negative displacements rotate to the right.
+	 */
+	function lrotate(x: number, disp: number): number;
+
+	/**
+	 * Returns the number `x` shifted `disp` bits to the left. The number `disp` may be any representable integer.
+	 * Negative displacements shift to the right. In any direction, vacant bits are filled with zeros.
+	 * In particular, displacements with absolute values higher than 31 result in zero (all bits are shifted out).
+	 *
+	 * For positive displacements, the following equality holds:
+	 *
+	 * `assert(bit32.lshift(b, disp) == (b * 2^disp) % 2^32)`
+	 */
+	function lshift(x: number, disp: number): number;
+
+	/**
+	 * Returns the number `x` rotated `disp` bits to the right. The number `disp` may be any representable integer.
+	 *
+	 * For any valid displacement, the following identity holds:
+	 *
+	 * `assert(bit32.rrotate(x, disp) == bit32.rrotate(x, disp % 32))`
+	 *
+	 * In particular, negative displacements rotate to the left.
+	 */
+	function rrotate(x: number, disp: number): number;
+
+	/**
+	 * Returns the number `x` shifted `disp` bits to the right. The number `disp` may be any representable integer.
+	 * Negative displacements shift to the left. In any direction, vacant bits are filled with zeros.
+	 * In particular, displacements with absolute values higher than 31 result in zero (all bits are shifted out).
+	 *
+	 * For positive displacements, the following equality holds:
+	 *
+	 * `assert(bit32.rshift(b, disp) == math.floor(b % 2^32 / 2^disp))`
+	 *
+	 * This shift operation is what is called logical shift.
+	 */
+	function rshift(x: number, disp: number): number;
+}
+
 declare namespace table {
 	/** Sorts list elements in a given order, in-place, from `list[1]` to `list[#list]`. Comp is a function that receives two list elements and returns true when the first element must come before the second in the final order (so that `not comp(list[i+1],list[i])` will be true after the sort). */
 	function sort<T>(t: Array<T>, comp?: (a: T, b: T) => boolean): void;
