95f367dfcf016af49ac251679f8b2a9a85f430e3
Niles <Validarkness@gmail.com>
2019-08-31 05:53:59 -0500

    Add Parameters/ConstructorParameters from lib.es5.d.ts (#151)
    Add Parameters/ConstructorParameters from lib.es5.d.ts

--- include/es.d.ts
+++ include/es.d.ts
@@ -909,6 +909,16 @@ type Extract<T, U> = T extends U ? T : never;
 type NonNullable<T> = T extends null | undefined ? never : T;
 
 /**
+ * Obtain the parameters of a function type in a `tuple | never`.
+ */
+type Parameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
+
+/**
+ * Obtain the parameters of a constructor function type in a `tuple | never`
+ */
+type ConstructorParameters<T extends new (...args: any) => any> = T extends new (...args: infer P) => any ? P : never;
+
+/**
  * Obtain the return type of a function type
  */
 type ReturnType<T extends (...args: Array<any>) => any> = T extends (...args: Array<any>) => infer R ? R : any;
