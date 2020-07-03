7d91610154a6563a26237bfd79df0fb6a59d357e
OverHash <46231745+OverHash@users.noreply.github.com>
2020-07-03 15:25:16 +1200

    include documentation

--- include/lua.d.ts
+++ include/lua.d.ts
@@ -176,6 +176,7 @@ interface DateTable {
 }
 
 declare namespace os {
+	/** Returns the number of seconds of CPU time for the program. It's typical use is to benchmark a piece of code. */
 	function clock(): number;
 	/** Returns the current number of seconds since Jan 1, 1970 in the UTC timezone. */
 	function time(): number;
