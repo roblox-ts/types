0669031167582134076200b675e986faec09c7f7
OverHash <46231745+OverHash@users.noreply.github.com>
2020-07-20 11:33:00 +1200

    Add custom InputObject.Position types

--- include/customDefinitions.d.ts
+++ include/customDefinitions.d.ts
@@ -295,6 +295,28 @@ interface HumanoidDescription extends Instance {
 	readonly EquippedEmotesChanged: RBXScriptSignal<(newEquippedEmotes: EquippedEmotes) => void>;
 }
 
+interface InputObject extends Instance {
+	/** This property describes a [Vector3](https://developer.roblox.com/search#stq=Vector3) positional value of this input.
+	 * 
+	 * For mouse and touch input, this is the screen position of the mouse/touch, described in the x and y components.
+	 * 
+	 * For the mouse wheel input, the z component describes whether the wheel was moved forward (1), backwards (-1), or not at all (0).
+	 * 
+	 * For [KeyCode](https://developer.roblox.com/search#stq=KeyCode) input, this indicate's the position of the player's `Mouse`.
+	 * 
+	 * #See also
+	 * 
+	 *  - [InputObject.Delta](https://developer.roblox.com/api-reference/property/InputObject/Delta)
+	 * 
+	 *  - [InputObject.KeyCode](https://developer.roblox.com/api-reference/property/InputObject/KeyCode)
+	 * 
+	 *  - [InputObject.UserInputState](https://developer.roblox.com/api-reference/property/InputObject/UserInputState)
+	 * 
+	 *  - [InputObject.UserInputType](https://developer.roblox.com/api-reference/property/InputObject/UserInputType)
+	 */
+	Position: Vector3;
+}
+
 interface InsertService extends Instance {
 	LoadAsset(this: InsertService, assetId: number): Model;
 	LoadAssetVersion(this: InsertService, assetVersionId: number): Model;
