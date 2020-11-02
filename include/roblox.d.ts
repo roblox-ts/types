0cff167c70deea809fc22d60308a94a0f7a3f74d
Osyris <osyrisrblx@gmail.com>
2020-11-01 22:06:59 -0800

    Update include/roblox.d.ts

--- include/roblox.d.ts
+++ include/roblox.d.ts
@@ -1074,7 +1074,7 @@ interface CFrameConstructor {
 	fromOrientation: (rX: number, rY: number, rZ: number) => CFrame;
 	/**
 	 * Creates a new CFrame located at `at` and facing towards `lookAt`, optionally specifying the upward direction (by default, (0, 1, 0)).
-	 * 
+	 *
 	 * This function replaces the `CFrame.new(Vector3, Vector3)` constructor which accomplished a similar task. This function allows you to specify the `up` Vector, using the same default as the old constructor.
 	 */
 	lookAt: (at: Vector3, lookAt: Vector3, up?: Vector3) => CFrame;
