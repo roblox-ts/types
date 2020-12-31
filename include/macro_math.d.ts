df81329922024bbbc4f67365b8ed34167012fa61
Validark <Validarkness@gmail.com>
2020-12-31 02:44:20 -0600

    Update macro_math.d.ts

--- include/macro_math.d.ts
+++ include/macro_math.d.ts
@@ -4,67 +4,67 @@
 
 declare interface CFrame {
 	/** macro for the `+` operator */
-	add(this: CFrame, v3: Vector3): CFrame;
+	add(this: CFrame, op: Vector3): CFrame;
 	/** macro for the `-` operator */
-	sub(this: CFrame, v3: Vector3): CFrame;
+	sub(this: CFrame, op: Vector3): CFrame;
 	/** macro for the `*` operator */
-	mul(this: CFrame, cf: CFrame | Vector3): CFrame;
+	mul<T extends CFrame | Vector3>(this: CFrame, op: T): T;
 }
 
 declare interface UDim {
 	/** macro for the `+` operator */
-	add(this: UDim, udim: UDim): UDim;
+	add(this: UDim, op: UDim): UDim;
 	/** macro for the `-` operator */
-	sub(this: UDim, udim: UDim): UDim;
+	sub(this: UDim, op: UDim): UDim;
 }
 
 declare interface UDim2 {
 	/** macro for the `+` operator */
-	add(this: UDim2, udim2: UDim2): UDim2;
+	add(this: UDim2, op: UDim2): UDim2;
 	/** macro for the `-` operator */
-	sub(this: UDim2, udim2: UDim2): UDim2;
+	sub(this: UDim2, op: UDim2): UDim2;
 }
 
 declare interface Vector2 {
 	/** macro for the `+` operator */
-	add(this: Vector2, v2: Vector2): Vector2;
+	add(this: Vector2, op: Vector2): Vector2;
 	/** macro for the `-` operator */
-	sub(this: Vector2, v2: Vector2): Vector2;
+	sub(this: Vector2, op: Vector2): Vector2;
 	/** macro for the `*` operator */
-	mul(this: Vector2, v2: Vector2 | number): Vector2;
+	mul(this: Vector2, op: Vector2 | number): Vector2;
 	/** macro for the `/` operator */
-	div(this: Vector2, v2: Vector2 | number): Vector2;
+	div(this: Vector2, op: Vector2 | number): Vector2;
 }
 
 declare interface Vector2int16 {
 	/** macro for the `+` operator */
-	add(this: Vector2int16, v2: Vector2int16): Vector2int16;
+	add(this: Vector2int16, op: Vector2int16): Vector2int16;
 	/** macro for the `-` operator */
-	sub(this: Vector2int16, v2: Vector2int16): Vector2int16;
+	sub(this: Vector2int16, op: Vector2int16): Vector2int16;
 	/** macro for the `*` operator */
-	mul(this: Vector2int16, v2: Vector2int16): Vector2int16;
+	mul(this: Vector2int16, op: Vector2int16): Vector2int16;
 	/** macro for the `/` operator */
-	div(this: Vector2int16, v2: Vector2int16): Vector2int16;
+	div(this: Vector2int16, op: Vector2int16): Vector2int16;
 }
 
 declare interface Vector3 {
 	/** macro for the `+` operator */
-	add(this: Vector3, v3: Vector3): Vector3;
+	add(this: Vector3, op: Vector3): Vector3;
 	/** macro for the `-` operator */
-	sub(this: Vector3, v3: Vector3): Vector3;
+	sub(this: Vector3, op: Vector3): Vector3;
 	/** macro for the `*` operator */
-	mul(this: Vector3, v3: Vector3 | number): Vector3;
+	mul(this: Vector3, op: Vector3 | number): Vector3;
 	/** macro for the `/` operator */
-	div(this: Vector3, v3: Vector3 | number): Vector3;
+	div(this: Vector3, op: Vector3 | number): Vector3;
 }
 
 declare interface Vector3int16 {
 	/** macro for the `+` operator */
-	add(this: Vector3int16, v3: Vector3int16): Vector3int16;
+	add(this: Vector3int16, op: Vector3int16): Vector3int16;
 	/** macro for the `-` operator */
-	sub(this: Vector3int16, v3: Vector3int16): Vector3int16;
+	sub(this: Vector3int16, op: Vector3int16): Vector3int16;
 	/** macro for the `*` operator */
-	mul(this: Vector3int16, v3: Vector3int16): Vector3int16;
+	mul(this: Vector3int16, op: Vector3int16): Vector3int16;
 	/** macro for the `/` operator */
-	div(this: Vector3int16, v3: Vector3int16): Vector3int16;
+	div(this: Vector3int16, op: Vector3int16): Vector3int16;
 }
