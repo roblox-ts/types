15e2e09380c79055189d732b564838bb1fbfeb77
Marcus <ukendio@gmail.com>
2021-05-29 22:50:29 +0200

    Update lua.d.ts

--- include/lua.d.ts
+++ include/lua.d.ts
@@ -107,12 +107,10 @@ declare function xpcall<T extends Array<any>, U>(
 
 /** Calls the function func with the given arguments in protected mode. This means that any error inside func is not propagated; instead, xpcall catches the error and returns a status code. Its first result is the status code (a boolean), which is true if the call succeeds without errors. In such case, xpcall also returns all results from the call, after this first result. In case of any error, pcall returns false plus the error message. */
 declare function xpcall<T extends any[], U, V>(
-    func: (...args: T) => U,
-    errHandler: (err: any) => V,
-    ...args: T
-): LuaTuple<U extends [...infer W]
-    ? [true, ...W] | [false, V]
-    : [true, U] | [false, V]>;
+	func: (...args: T) => U,
+	errHandler: (err: any) => V,
+	...args: T
+): LuaTuple<U extends [...infer W] ? [true, ...W] | [false, V] : [true, U] | [false, V]>;;
 
 interface LuaMetatable<T> {
 	__index?: (self: T, index: unknown) => void;
