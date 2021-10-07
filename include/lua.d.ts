9485f7cbfd5f6a699820402caf9f8f62bd79896f
Dion <39664950+Dionysusnu@users.noreply.github.com>
2021-10-07 23:02:27 +0200

    Update lua.d.ts (#495)

--- include/lua.d.ts
+++ include/lua.d.ts
@@ -673,6 +673,10 @@ declare namespace bit32 {
 declare namespace table {
 	/** Sorts list elements in a given order, in-place, from `list[1]` to `list[#list]`. Comp is a function that receives two list elements and returns true when the first element must come before the second in the final order (so that `not comp(list[i+1],list[i])` will be true after the sort). */
 	function sort<T>(t: Array<T>, comp?: (a: T, b: T) => boolean): void;
+	/** Makes the table read-only and prohibits all further modifications */
+	function freeze(t: object): void;
+	/** Returns whether or not `t` is frozen */
+	function isfrozen<T extends object>(t: T): t is Readonly<T>;
 }
 
 type thread = {
