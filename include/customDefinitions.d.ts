e2cf21a6efc825695975ab3f24a9fe6feff55296
osyrisrblx <osyrisrblx@gmail.com>
2022-04-24 16:07:35 -0400

    Fix eslint warnings

--- include/customDefinitions.d.ts
+++ include/customDefinitions.d.ts
@@ -764,7 +764,13 @@ interface TeleportService extends Instance {
 	readonly LocalPlayerArrivedFromTeleport: RBXScriptSignal<(loadingGui: ScreenGui, dataTable?: unknown) => void>;
 
 	readonly TeleportInitFailed: RBXScriptSignal<
-		(player: Player, teleportResult: Enum.TeleportResult, errorMessage: string, placeId: number, teleportOptions: TeleportOptions) => void
+		(
+			player: Player,
+			teleportResult: Enum.TeleportResult,
+			errorMessage: string,
+			placeId: number,
+			teleportOptions: TeleportOptions,
+		) => void
 	>;
 	/** @server */
 	GetPlayerPlaceInstanceAsync(this: TeleportService, userId: number): LuaTuple<[boolean, string, number, string]>;
