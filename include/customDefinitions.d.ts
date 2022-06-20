f773d8f1b35d59080f87df4c6fbe654ff60ff258
osyrisrblx <osyrisrblx@gmail.com>
2022-06-20 15:43:35 -0400

    Add `AttributeValue` type

--- include/customDefinitions.d.ts
+++ include/customDefinitions.d.ts
@@ -418,6 +418,9 @@ interface Instance {
 		this: T,
 		propertyName: InstancePropertyNames<T>,
 	): RBXScriptSignal<() => void>;
+	GetAttribute(this: Instance, attribute: string): AttributeValue;
+	SetAttribute(this: Instance, attribute: string, value: AttributeValue): void;
+	GetAttributes(this: Instance): Map<string, AttributeValue>;
 	readonly AncestryChanged: RBXScriptSignal<(child: Instance, parent: Instance | undefined) => void>;
 }
 
