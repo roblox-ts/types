133752c9ae52842daef2194a7174c8f853a05b1b
Sasial <44125644+sasial-dev@users.noreply.github.com>
2023-11-30 12:28:57 +1000

    Add typings for UnreliableRemoteEvent

--- include/customDefinitions.d.ts
+++ include/customDefinitions.d.ts
@@ -779,6 +779,25 @@ interface RemoteEvent<T extends Callback = Callback> extends BaseRemoteEvent {
 	FireServer(this: RemoteEvent, ...args: Parameters<T>): void;
 }
 
+interface UnreliableRemoteEvent<T extends Callback = Callback> extends BaseRemoteEvent {
+	readonly OnClientEvent: RBXScriptSignal<T>;
+	/** The reason we DON'T allow you to use `Parameters<T>` here is because you can't trust data from the client. Please type-check and sanity-check all values received from the client. E.g. if you are expecting a number from the client, you should check whether the received value is indeed a number and you might also want to make sure it isn't a `NaN` value. See example code:
+	 * ```ts
+	 * (new Instance("RemoteEvent") as RemoteEvent<(num: number) => void>).OnServerEvent.Connect((plr, num) => {
+	 *     if (typeIs(num, "number") && num === num) {
+	 *         print(`Yay! Valid number: ${num}`);
+	 *     } else {
+	 *         print(`Bad argument received from ${plr.Name}! Exploit or bug?`);
+	 *     }
+	 * });
+	 * ```
+	 */
+	readonly OnServerEvent: RBXScriptSignal<(player: Player, ...args: Array<unknown>) => void>;
+	FireAllClients(this: RemoteEvent, ...args: Parameters<T>): void;
+	FireClient(this: RemoteEvent, player: Player, ...args: Parameters<T>): void;
+	FireServer(this: RemoteEvent, ...args: Parameters<T>): void;
+}
+
 interface RemoteFunction<T extends Callback = Callback> extends Instance {
 	OnClientInvoke: T | undefined;
 	/** The reason we DON'T allow you to use `Parameters<T>` here is because you can't trust data from the client. Please type-check and sanity-check all values received from the client. E.g. if you are expecting a number from the client, you should check whether the received value is indeed a number and you might also want to make sure it isn't a `NaN` value. See example code:
