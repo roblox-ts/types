e6c8702a9e5df5e86de38fc48bb666fe9fd5d88d
osyrisrblx <osyrisrblx@gmail.com>
2023-11-30 00:43:51 -0500

    RemoteEvent -> UnreliableRemoteEvent

--- include/customDefinitions.d.ts
+++ include/customDefinitions.d.ts
@@ -783,7 +783,7 @@ interface UnreliableRemoteEvent<T extends Callback = Callback> extends BaseRemot
 	readonly OnClientEvent: RBXScriptSignal<T>;
 	/** The reason we DON'T allow you to use `Parameters<T>` here is because you can't trust data from the client. Please type-check and sanity-check all values received from the client. E.g. if you are expecting a number from the client, you should check whether the received value is indeed a number and you might also want to make sure it isn't a `NaN` value. See example code:
 	 * ```ts
-	 * (new Instance("RemoteEvent") as RemoteEvent<(num: number) => void>).OnServerEvent.Connect((plr, num) => {
+	 * (new Instance("UnreliableRemoteEvent") as UnreliableRemoteEvent<(num: number) => void>).OnServerEvent.Connect((plr, num) => {
 	 *     if (typeIs(num, "number") && num === num) {
 	 *         print(`Yay! Valid number: ${num}`);
 	 *     } else {
@@ -793,9 +793,9 @@ interface UnreliableRemoteEvent<T extends Callback = Callback> extends BaseRemot
 	 * ```
 	 */
 	readonly OnServerEvent: RBXScriptSignal<(player: Player, ...args: Array<unknown>) => void>;
-	FireAllClients(this: RemoteEvent, ...args: Parameters<T>): void;
-	FireClient(this: RemoteEvent, player: Player, ...args: Parameters<T>): void;
-	FireServer(this: RemoteEvent, ...args: Parameters<T>): void;
+	FireAllClients(this: UnreliableRemoteEvent, ...args: Parameters<T>): void;
+	FireClient(this: UnreliableRemoteEvent, player: Player, ...args: Parameters<T>): void;
+	FireServer(this: UnreliableRemoteEvent, ...args: Parameters<T>): void;
 }
 
 interface RemoteFunction<T extends Callback = Callback> extends Instance {
