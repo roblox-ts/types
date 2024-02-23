a74d576f53366b0cbb9cab8b74fa7207e36b3a59
Neohertz <rpate5282@hotmail.com>
2024-02-23 14:59:36 -0700

    Alphabetical definitions

--- include/customDefinitions.d.ts
+++ include/customDefinitions.d.ts
@@ -66,6 +66,14 @@ interface Attachment extends Instance {
 	WorldCFrame: CFrame;
 }
 
+interface AudioAnalyzer extends Instance {
+	/**
+	 * Returns the frequency spectrum of the last audio buffer, as an array of numbers. The elements of the array are root-mean-square volume levels, evenly spaced from 0 hertz to 24,000 hertz.
+	 * @returns number[512]
+	 */
+	GetSpectrum(this: AudioAnalyzer): Array<number>;
+}
+
 interface AvatarEditorService extends Instance {
 	GetAvatarRules(this: AvatarEditorService): AvatarRules;
 	GetBatchItemDetails(
@@ -866,14 +874,6 @@ interface ServiceProvider<S = unknown> extends Instance {
 
 interface SocialService extends Instance {
 	readonly GameInvitePromptClosed: RBXScriptSignal<(senderPlayer: Player, recipientIds: Array<number>) => void>;
-}
-
-interface AudioAnalyzer extends Instance {
-	/**
-	 * Returns the frequency spectrum of the last audio buffer, as an array of numbers. The elements of the array are root-mean-square volume levels, evenly spaced from 0 hertz to 24,000 hertz.
-	 * @returns number[512]
-	 */
-	GetSpectrum(this: AudioAnalyzer): Array<number>;
 }
 
 interface SoundService extends Instance {
