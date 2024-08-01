cf31d9a8c9893883147ba618105ce0576000ea71
Catargo <64728790+Velover@users.noreply.github.com>
2024-08-01 12:37:26 +0200

    fix fromExisting in InstanceConstructor
    the way with keyof CreatableInstances doesnt work
    + Some instances will have to be casted because e.g. BasePart is not creatable etc.

--- include/roblox.d.ts
+++ include/roblox.d.ts
@@ -817,9 +817,9 @@ interface InstanceConstructor {
 	 * - This constructor will not copy any of the descendant `Instances` parented to the existing object.
 	 * - This constructor will return a new object even if the existing object had `Instance.Archivable` set to `false`.
 	 */
-	fromExisting: <T extends keyof CreatableInstances>(
-		existingInstance: CreatableInstances[T],
-	) => CreatableInstances[T];
+	fromExisting: <T extends Instance>(
+		existingInstance: T,
+	) => T;
 }
 
 declare const Instance: InstanceConstructor;
