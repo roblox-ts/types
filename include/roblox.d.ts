98080f23d7f1d429d3510216e8a091d0cfe1e8f5
igz <jordenwolf05@gmail.com>
2025-01-20 10:55:53 -0500

    Remove param description

--- include/roblox.d.ts
+++ include/roblox.d.ts
@@ -1376,9 +1376,7 @@ interface CFrame {
 	ToOrientation(this: CFrame): LuaTuple<[number, number, number]>;
 	/** Returns a tuple of a Vector3 and a number which represent the rotation of the CFrame in the axis-angle representation */
 	ToAxisAngle(this: CFrame): LuaTuple<[Vector3, number]>;
-	/** Returns true if the other CFrame is sufficiently close to this CFrame in both position and rotation. The eps value is used to control the tolerance for this similarity. This value is optional and should be a small positive value if provided. The similarity for position is component-wise, and for rotation uses a fast approximation of the angle difference.
-	 @param epsilon Default: 1e-5
-	*/
+	/** Returns true if the other CFrame is sufficiently close to this CFrame in both position and rotation. The eps value is used to control the tolerance for this similarity. This value is optional and should be a small positive value if provided. The similarity for position is component-wise, and for rotation uses a fast approximation of the angle difference. */
 	FuzzyEq(this: CFrame, other: CFrame, epsilon?: number): boolean;
 }
 
